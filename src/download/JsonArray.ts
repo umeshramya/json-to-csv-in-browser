278a522ffb069a02f8d0da1ddf499e62f2ed4e0b
pradyumnabilagi <pradyumnabilagi12@gmail.com>
2022-04-03 18:20:26 +0530

    done

--- src/download/JsonArray.ts
+++ src/download/JsonArray.ts
@@ -1,38 +1,33 @@
-export class JsonArray{
-    private _JsonArray:any[]
-    private _csvString!:string;
-    private _headers!:string[];
+export class JsonArray {
+  private _JsonArray: any[];
+  private _csvString!: string;
+  private _headers!: string[];
 
-    constructor(JsonArray:any[]){
-        this._JsonArray =JsonArray;
-       this.setHeaders();
-    }
+  constructor(JsonArray: any[]) {
+    this._JsonArray = JsonArray;
+    this.setHeaders();
+  }
 
-    public convertToCSVstring():string{
-        let str:string = this._headers.toString();
-            this._JsonArray.forEach(arr=>{
-                let line:string=""
-                this._headers.forEach(head=>{
-                    let val:string = arr[head] === undefined ? "" : arr[head];
-                    val = val.toString().search(",") >= 0  ? `"${val}"` : val
-                    if(line.length){
-                        
-                        line=`${line}, ${val}`;
-                    }else{
-                        line=`${val === undefined ? "" : val}`
-                    }
-                    
-                })
-                str= str + "\n" + line;
-            })
+  public convertToCSVstring(): string {
+    let str: string = this._headers.toString();
+    this._JsonArray.forEach((arr) => {
+      let line: string = "";
+      this._headers.forEach((head) => {
+        let val: string = arr[head] == undefined ? "" : arr[head];
+        val = val.toString().search(",") >= 0 ? `"${val}"` : val;
+        if (line.length) {
+          line = `${line}, ${val}`;
+        } else {
+          line = `${val === undefined ? "" : val}`;
+        }
+      });
+      str = str + "\n" + line;
+    });
 
-        return str;
+    return str;
+  }
 
-    }
-
-    private setHeaders(){ 
-        this._headers= Object.keys(this._JsonArray[0]).map(key=>key)
-    }
+  private setHeaders() {
+    this._headers = Object.keys(this._JsonArray[0]).map((key) => key);
+  }
 }
-
-
